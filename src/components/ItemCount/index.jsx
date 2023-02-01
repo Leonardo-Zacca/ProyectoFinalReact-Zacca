4a708da06dce11853de7f3c0ee1a375ce1acfa8e
Leonardo-Zacca <leonardozacca@hotmail.com>
2023-02-01 17:28:26 -0300

    agregando item count para productos

---
+++ src/components/ItemCount/index.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./styles.scss";
+
+const ItemCount = ({ onAdd, stock, initial }) => {
+    const [count, setCount] = useState(initial);
+
+    const onPlus = () => {
+        if (count < stock) setCount(count + 1);
+    };
+
+    const onDecrement = () => {
+        if (count > initial) setCount(count - 1);
+    };
+
+    return (
+        <div className="count-container">
+            <button className="boton" onClick={onDecrement}>-</button>
+            <span className="boton">{count}</span>
+            <button className="boton" onClick={onPlus}>+</button>
+            <button className="boton" onClick={() => onAdd(count)}>Confirmar compra</button>
+        </div>
+    );
+};
+
+export default ItemCount;
--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,19 +1,40 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ItemCount from "../ItemCount";
+import "./styles.scss";
 
-const ItemDetail = ({detail}) => {
-    console.log(detail);
+const ItemDetail = ({ detail }) => {
+
+    const [quantity, setQuantity] = useState(0)
+
+    const onAdd = (cantidad) => {
+        console.log(`Se agregó una cantidad de productos: ${cantidad}`)
+        setQuantity(cantidad)
+    }
+
+    console.log(detail.title);
     return (
-        <div>
-        {'Producto: '}
-        {detail.title}
-        {' | Precio :'}
-        {detail.price}
-        {' | Descripcion:'}
-        {detail.description}
+        <div className="detail-container">
+            <img className="detail-img" src={detail.image} alt="detalle" />
+            <aside className="detail-aside">
+                <h4>{detail.title}</h4>
+                {
+                    quantity === 0 ?
+                    <ItemCount 
+                        stock={20} 
+                        initial={1} 
+                        onAdd={onAdd}    
+                    />
+                    :
+                    <button className="boton">
+                        <Link to="/cart">
+                            Ir al carrito
+                        </Link>
+                    </button>
+                }
+            </aside>
         </div>
-    )
-    
+    );
+};
 
-}
-
-export default ItemDetail
+export default ItemDetail;
