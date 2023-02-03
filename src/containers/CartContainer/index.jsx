6bf29648faffd6db05f00b56d5d7c5a453cbd09b
Leonardo-Zacca <leonardozacca@hotmail.com>
2023-02-03 19:07:58 -0300

    creando carrito

--- src/containers/CartContainer/index.jsx
+++ src/containers/CartContainer/index.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import TableRow from './TableRow'
+import { Shop} from '../../context/ShopProvider'
 
 const Cart = () => {
+
+  const {products} = useContext(Shop);
+
   return (
-    <div>Cart</div>
+    <table class="table table-success table-striped">
+      <thead>
+        <tr>
+          <th scope="col">id</th> 
+          <th scope="col">Image</th>
+          <th scope="col">title</th>
+          <th scope="col">price</th>
+          <th scope="col">quantity</th>
+          <th scope="col">Remove</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map(product => {
+          return <TableRow key={product.id} product={product} />
+        })}
+      </tbody>
+    </table>
   )
 }
 
