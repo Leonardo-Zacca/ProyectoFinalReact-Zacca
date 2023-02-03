2e84fbfcab427b86142a958fdf8674b21e4d75fa
Leonardo-Zacca <leonardozacca@hotmail.com>
2023-02-03 03:04:12 -0300

    arreglando contador de productos

--- src/context/ShopProvider.jsx
+++ src/context/ShopProvider.jsx
@@ -11,11 +11,35 @@ const ShopProvider = ({children}) => {
     const [products, setProducts] = useState([])
 
     const addProduct = (product) => {
-        setProducts([...products, product])
+        const isInCart = isProductInCart(product.id)
+        if (isInCart) {
+
+            //hace algo
+            //Primero vamos a encontrar el producto repetido
+            const productoRepetido = products.find(element => element.id === product.id)
+            productoRepetido.quantity += product.quantity
+            setProducts([...products])
+            
+        } else {
+            setProducts([...products, product])
+        }
+
+    }
+
+    const countCart = () => {
+        let cantidadTotal = 0;
+        for (const product of products) {
+            cantidadTotal += product.quantity  //recordad que el += permite sumar al valor original la nueva cantidad 
+        }
+        return cantidadTotal
+    }
+
+    const isProductInCart = (id) => {
+        return products.some(product => product.id === id)
     }
 
     return (
-        <Shop.Provider value = {{products , addProduct}}>  
+        <Shop.Provider value = {{products , addProduct, countCart}}>  
             {children}
         </Shop.Provider>
   )
