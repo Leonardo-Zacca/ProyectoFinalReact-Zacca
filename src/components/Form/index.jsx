cc380ae76144924f1147ada6063235a95e92a612
Leonardo-Zacca <leonardozacca@hotmail.com>
2023-02-08 20:21:22 -0300

    agregando mejoras

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
+import "./styles.css" 
 
 const FormComp = ({ confirmPurchase, formVis, setFormVis }) => {
   const {
@@ -22,7 +23,7 @@ const FormComp = ({ confirmPurchase, formVis, setFormVis }) => {
     <>
       <Modal show={formVis} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Checkout</Modal.Title>
+          <Modal.Title>Checkout Compra</Modal.Title>
         </Modal.Header>
         <form onSubmit={handleSubmit(onSubmit)}>
           <Modal.Body>
@@ -34,10 +35,10 @@ const FormComp = ({ confirmPurchase, formVis, setFormVis }) => {
               })}
             />
             {errors?.nombre?.type === "required" && (
-              <p>El campo nombre es requerido</p>
+              <p className="error">El campo nombre es requerido</p>
             )}
             {errors?.nombre?.type === "minLength" && (
-              <p>El nombre debe superar los 2 caracteres</p>
+              <p className="error">El nombre debe superar los 2 caracteres</p>
             )}
             <label>Email</label>
             <input
@@ -45,10 +46,10 @@ const FormComp = ({ confirmPurchase, formVis, setFormVis }) => {
               {...register("email", { minLength: 3, required: true })}
             />
             {errors?.email?.type === "minLength" && (
-              <p>El mail tiene que tener minimo 3 caracteres</p>
+              <p className="error">El mail tiene que tener minimo 3 caracteres</p>
             )}
             {errors?.email?.type === "required" && (
-              <p>El campo email es requerido</p>
+              <p className="error">El campo email es requerido</p>
             )}
             <label>Telefono</label>
             <input
@@ -56,10 +57,10 @@ const FormComp = ({ confirmPurchase, formVis, setFormVis }) => {
               {...register("phone", { min: 10, maxLength: 10, required: true })}
             />
             {errors?.phone?.type === "minLength" && (
-              <p>El teléfono debe tener 10 digitos</p>
+              <p className="error">El teléfono debe tener 10 digitos</p>
             )}
             {errors?.phone?.type === "required" && (
-              <p>El campo telefono es requerido</p>
+              <p className="error">El campo telefono es requerido</p>
             )}
           </Modal.Body>
           <Modal.Footer>
